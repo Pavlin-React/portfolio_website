280223ea6e18fe456ed965a1aef9d9a46878257b
Pavlin Ivanov <pavlinivanov9@gmail.com>
2021-10-14 15:31:36 +0300

    react-icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiFillTwitterSquare, AiFillLinkedin } from 'react-icons/ai';
 
 import { SocialIcons } from '../Header/HeaderStyles';
 import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
@@ -31,7 +31,7 @@ const Footer = () => {
             <AiFillLinkedin size="3rem" />
           </SocialIcons>
           <SocialIcons href="https://google.com">
-            <AiFillInstagram size="3rem" />
+            <AiFillTwitterSquare size="3rem" />
           </SocialIcons>
         </SocialContainer>
       </SocialIconsContainer>
